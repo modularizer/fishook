8e3df080a45fe7cdc1218e72e1744449857f46fe
mod <modularizer@gmail.com>
2026-01-09 17:50:29 -0800

    test version

--- common/forbid-pattern.sh
+++ common/forbid-pattern.sh
@@ -9,6 +9,8 @@ MESSAGE="${2:-contains forbidden pattern}"
 
 [[ -z "$PATTERN" ]] && echo "Usage: forbid-pattern.sh <pattern> <message>" >&2 && exit 1
 
+echo "checking $(new)"
+
 if new | grep -qE "$PATTERN"; then
   raise "$MESSAGE"
 fi
